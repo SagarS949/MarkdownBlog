60072516c3de28f072599a90dc4ff970b477cb3f
Sagar S <sagarsubramanian2014@gmail.com>
2023-07-02 18:17:15 +0530

    views for articles and db connected

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const articleRouter = require('./routes/articles')
 const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
+const Article = require('./models/article')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost/blog',{
+    useNewUrlparser: true, useUnifiedTopology: true
+})
 
 //views written using ejs,view engine converts ejs to html
 app.set('view engine','ejs')
 
+app.use(express.urlencoded({extended: false}))
 app.use('/articles',articleRouter)
 
-app.get('/',(req,res)=>{
-    const articles = [{
-        title: "Test Article",
-        date: new Date(),
-        description: "Test description"
-    },{
-        title: "Test Article2",
-        date: new Date(),
-        description: "Test description2"
-    }]
-    res.render('index', {articles:articles})
+app.get('/',async (req,res)=>{
+    const articles = await Article.find().sort({createdAt: 'desc'})
+    res.render('articles/index', {articles:articles})
 })
 app.listen(5000, ()=>{
     console.log("Server started on port 5000");
--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,39 @@
 const express = require('express')
+const Article = require('./../models/article')
 const router = express.Router()
 
-router.get('/test', (req,res)=>{
-    res.send("hello")
+router.get('/new', (req,res)=>{
+    // sends a new article to be rendered(will be empty for all fields)
+    res.render("articles/new", {article: new Article()})
+})
+
+router.get('/:id',async (req,res)=>{
+    const article = await Article.findById(req.params.id)
+    if(article == null){
+        res.redirect('/')
+    }
+    //goes to show.ejs to display article
+    res.render('articles/show',{article: article})
+})
+
+//called when form is submitted to create new article from new.ejs
+router.post('/',async (req,res)=>{
+    let article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    try{
+        article = await article.save()
+        console.log(article.id)
+        //redirect to display article that was just created
+        res.redirect(`articles/${article.id}`)
+    }catch (err){
+        console.log(err)
+        //done to prefill fields with already prev entered data
+        res.render('articles/new',{article: article})
+    }
+
 })
 
 module.exports = router
